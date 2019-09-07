5ae239a303af3e55f13aa2fc5f0649b72b44ba11
Prithaj Nath <prithajnath@gmail.com>
2019-09-07 19:15:19 -0400

    Fix module names

--- angrydevblog/celery.py
+++ angrydevblog/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'theangrydevblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'angrydevblog.settings')
 
-app = Celery('theangrydevblog')
+app = Celery('angrydevblog')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
