7493fc8a34cc8b126cda32ae381238a5a2ff0f7c
Prithaj Nath <prithajnath@gmail.com>
2019-09-07 17:51:40 -0400

    Replace copypasta

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ COMMIT_SHA1=$CIRCLE_SHA1
 # We must export it so it's available for envsubst
 export COMMIT_SHA1=$COMMIT_SHA1
 
-envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
-mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
+envsubst <./.k8s/deployment.yml >./.k8s/deployment.yml.out
+mv ./.k8s/deployment.yml.out ./.k8s/deployment.yml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
