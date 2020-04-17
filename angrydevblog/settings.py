9c7b191513cfafd51bce6dd657547a44378f5e3e
Prithaj Nath <prithajnath@gmail.com>
2020-04-16 21:44:47 -0400

    Fix STATIC_URL

--- angrydevblog/settings.py
+++ angrydevblog/settings.py
@@ -124,5 +124,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_URL = f"{os.path.join(BASE_DIR, 'static')}/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
