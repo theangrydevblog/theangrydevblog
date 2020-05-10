8865b35a0289d5803821f83fe79647271bd60f96
Prithaj Nath <prithajnath@gmail.com>
2020-05-10 16:46:37 -0400

    Change post title style

--- angrydevblog/settings.py
+++ angrydevblog/settings.py
@@ -144,6 +144,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = f"{os.path.join(BASE_DIR, 'static')}/"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = f"{os.path.join(BASE_DIR, 'static')}/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
