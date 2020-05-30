f5b9c1c574e0e1a63e399aede9320d0839431ce2
Prithaj Nath <prithajnath@gmail.com>
2020-05-29 23:22:13 -0400

    Use Apollo React hooks for GraphQL

--- assets/js/components/comment.jsx
+++ assets/js/components/comment.jsx
@@ -1,19 +1,16 @@
-import React, { Component } from "react";
-export default class Comment extends Component{
+import React from "react";
 
-    // constructor(props) {
-    //   super(props);
-    //   this.state = {text: ""};
-    // }
+function Comment(props){
 
-
-  render() {
-    return <div className="comment-container nerd">
-        <p>{ this.props.text }</p>
+  return(
+    <div className="comment-container nerd">
+        <p>{ props.text }</p>
         <div style={{display: 'flex', paddingLeft: "80%", paddingRight: "5%"}}>
-            <img className="user-avatar" src={ this.props.avatar } />
-            <p style={{ paddingLeft: "6%", paddingTop: "2%", paddingRight: "2%" }}><strong>{ this.props.author }</strong></p>
+            <img className="user-avatar" src={ props.avatar } />
+            <p style={{ paddingLeft: "6%", paddingTop: "2%", paddingRight: "2%" }}><strong>{ props.author }</strong></p>
         </div>
-      </div>;
-  }
+    </div>
+  )
 }
+
+export default Comment
